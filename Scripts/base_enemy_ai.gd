36ca56709f8bf6502bd38fb5eea6a85cacbe3b87
Renato Costa <xmassiveg@gmail.com>
2024-10-07 15:19:12 -0300

    added health bar and basic enemy attack

--- Scripts/base_enemy_ai.gd
+++ Scripts/base_enemy_ai.gd
@@ -1,43 +1,52 @@
 extends CharacterBody2D
 class_name EnemyBase
 
-var speed :int = 250
-var health :int = 10
-var damage :int = 2
+#enemy attributes
+var speed: int = 250
+var health: int = 10
+var damage: int = 2
 
-var can_attack :bool = false
+#booleans
+var can_attack: bool = false
 
-var direction :Vector2
-var player_pos :Vector2
+var direction: Vector2
+var player_pos: Vector2
 
-@onready var attack_cd :Timer = $Attack_CD
-@onready var player :CharacterBody2D = get_tree().get_first_node_in_group("player")
+var player_node: CharacterBody2D
+
+@onready var attack_cd: Timer = $Attack_CD
+@onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
 
 func _physics_process(delta: float) -> void:
+	movement()
+	if can_attack == true:
+		attack()
+
+func movement():
 	player_pos = player.global_position
 	if !(global_position.distance_to(player_pos) < 15):
 		direction = global_position.direction_to(player_pos)
 		velocity = direction * speed
 	else:
 		velocity = Vector2.ZERO
-	
-	if can_attack == true:
-		attack()
-	
-	move_and_collide(velocity * delta)
+		
+	move_and_slide()
 
 func attack() -> void:
 	if attack_cd.is_stopped():
+		player_node.call("_take_damage", damage)
 		print("attack!")
-		attack_cd.start(2)
+		attack_cd.start(.15)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		player_node = body
 		print("ready")
 		can_attack = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		player_node = null
 		print("out of range")
 		can_attack = false
 		attack_cd.stop()
--- Scripts/player_script.gd
+++ Scripts/player_script.gd
@@ -1,9 +1,17 @@
 extends CharacterBody2D
 
-var speed :int = 400
+var speed: int = 400
+var max_health: int = 25
+var current_health: int = 25
 
-var direction :Vector2
-var mouse_pos :Vector2
+var direction: Vector2
+var mouse_pos: Vector2
+
+@onready var health_bar: TextureProgressBar = $TextureProgressBar
+
+func _ready() -> void:
+	health_bar.max_value = max_health
+	health_bar.value = current_health
 
 func _physics_process(delta: float) -> void:
 	movement()
@@ -16,3 +24,15 @@ func movement() -> void:
 	
 	#move the player
 	move_and_slide()
+
+func update_health_bar() -> void:
+	health_bar.max_value = max_health
+	health_bar.value = current_health
+
+func _take_damage(damage: int):
+	current_health -= damage
+	print(current_health)
+	update_health_bar()
+	if(current_health <= 0):
+		#call end game screen
+		get_tree().quit()
